525bc78679c304cd64fb218e8190502b51974a9a
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-03-14 21:43:24 +0100

    init

---
+++ nodejs/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("public/pages/frontpage/frontpage.html"));
+});
+
+const PORT = 8080;
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error);
+    } 
+    console.log(`Server is running on port ${PORT}`);
+});
